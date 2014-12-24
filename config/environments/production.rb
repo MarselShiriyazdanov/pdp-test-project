a0b90c5339f71d5d5245a10f7a785f792fa6f940
Marsel Shiriyazdanov <marsel.shiriyazdanov@flatstack.com>
2014-12-24 13:13:35 +0300

    hash fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,12 +79,12 @@ Rails.application.configure do
   config.host = 'still-spire-1631.herokuapp.com'
 
   ActionMailer::Base.smtp_settings = {
-    address        : 'smtp.sendgrid.net',
-    port           : '587',
-    authentication : :plain,
-    user_name      : ENV['SENDGRID_USERNAME'],
-    password       : ENV['SENDGRID_PASSWORD'],
-    domain         : 'heroku.com',
+    address: 'smtp.sendgrid.net',
+    port: '587',
+    authentication: :plain,
+    user_name: ENV['SENDGRID_USERNAME'],
+    password: ENV['SENDGRID_PASSWORD'],
+    domain: 'heroku.com',
     enable_starttls_auto: true
   }
 end
