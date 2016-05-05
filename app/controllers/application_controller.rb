15be41db4b46664b76451ee40807aa83112fff85
Marsel Shiriyazdanov <marsel.shiriyazdanov@flatstack.com>
2016-05-05 15:28:31 +0300

    allow everyone create articles

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,9 @@ class ApplicationController < ActionController::Base
 
   protect_from_forgery with: :exception
 
+  responders :flash
+  respond_to :html
+
   protected
 
   def devise_parameter_sanitizer
