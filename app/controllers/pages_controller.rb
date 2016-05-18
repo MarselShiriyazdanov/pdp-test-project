db67ec8be4834ce6e341376a498fe72ff6a86a2f
MarselShiriyazdanov <marsel.shiriyazdanov@flatstack.com>
2016-05-18 18:11:57 +0400

    add about_us page (#14)
    * add about_us page
    
    * rename dashboard controller

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,9 @@
-class DashboardController < ApplicationController
+class PagesController < ApplicationController
   expose(:articles) { Article.order(id: :desc).page(params[:page]).per(10) }
 
   def index
+  end
+
+  def about_us
   end
 end
